99079024e4d7446e79ea96d39fe7196510fce8dd
Nimsara Thisal Paramulla <85945422+nimsara1999@users.noreply.github.com>
2024-04-27 02:40:00 +0530

    Login and signup implemented

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import React from "react";
 import HomeScreen from "./screens/HomeScreen";
+import LoginScreen from "./screens/LoginScreen";
 import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <>
-      <StatusBar style="auto" />
-      <HomeScreen />
-    </>
+    <NavigationContainer>
+      <Stack.Navigator>
+      <Stack.Screen options={{ headerShown: false }} name="Login" component={LoginScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,136 +1,52 @@
-import React, { useState, useEffect, useRef } from "react";
-import { StyleSheet, Text, View, PermissionsAndroid } from "react-native";
-import { BleManager } from "react-native-ble-plx";
-import { atob } from "react-native-quick-base64";
+import { useNavigation } from '@react-navigation/core'
+import React from 'react'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { auth } from '../firebase'
 
-const bleManager = new BleManager();
+const HomeScreen = () => {
+  const navigation = useNavigation()
 
-async function requestLocationPermission() {
-  try {
-    const granted = await PermissionsAndroid.request(
-      PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION,
-      {
-        title: "Location permission for bluetooth scanning",
-        message:
-          "Grant location permission to allow the app to scan for Bluetooth devices",
-        buttonNeutral: "Ask Me Later",
-        buttonNegative: "Cancel",
-        buttonPositive: "OK",
-      }
-    );
-    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-      console.log("Location permission for bluetooth scanning granted");
-    } else {
-      console.log("Location permission for bluetooth scanning denied");
-    }
-  } catch (err) {
-    console.warn(err);
+  const handleSignOut = () => {
+    auth
+      .signOut()
+      .then(() => {
+        navigation.replace("Login")
+      })
+      .catch(error => alert(error.message))
   }
-}
-
-const SERVICE_UUID = "4fafc201-1fb5-459e-8fcc-c5c9c331914b";
-const STEP_DATA_CHAR_UUID = "beefcafe-36e1-4688-b7f5-00000000000b";
-
-export default function HomeScreen() {
-  const [deviceID, setDeviceID] = useState(null);
-  const [stepCount, setStepCount] = useState(0);
-  const [stepDataChar, setStepDataChar] = useState(null);
-  const [connectionStatus, setConnectionStatus] = useState("Searching...");
-
-  const deviceRef = useRef(null);
-
-  const searchAndConnectToDevice = () => {
-    bleManager.startDeviceScan(null, null, (error, device) => {
-      if (error) {
-        console.error(error);
-        setConnectionStatus("Error searching for devices");
-        return;
-      }
-      if (device.name === "Step-Sense") {
-        bleManager.stopDeviceScan();
-        setConnectionStatus("Connecting...");
-        connectToDevice(device);
-      }
-    });
-  };
-
-  useEffect(() => {
-    requestLocationPermission();
-    searchAndConnectToDevice();
-  }, []);
-
-  const connectToDevice = (device) => {
-    return device
-      .connect()
-      .then((device) => {
-        setDeviceID(device.id);
-        setConnectionStatus("Connected");
-        deviceRef.current = device;
-        return device.discoverAllServicesAndCharacteristics();
-      })
-      .then((device) => {
-        return device.services();
-      })
-      .then((services) => {
-        let service = services.find((service) => service.uuid === SERVICE_UUID);
-        return service.characteristics();
-      })
-      .then((characteristics) => {
-        let stepDataCharacteristic = characteristics.find(
-          (char) => char.uuid === STEP_DATA_CHAR_UUID
-        );
-        setStepDataChar(stepDataCharacteristic);
-        stepDataCharacteristic.monitor((error, char) => {
-          if (error) {
-            console.error(error);
-            return;
-          }
-          const rawStepData = atob(char.value);
-          console.log("Received step data:", rawStepData);
-          setStepCount(rawStepData);
-        });
-      })
-      .catch((error) => {
-        console.log(error);
-        setConnectionStatus("Error in Connection");
-      });
-  };
-
-  useEffect(() => {
-    const subscription = bleManager.onDeviceDisconnected(
-      deviceID,
-      (error, device) => {
-        if (error) {
-          console.log("Disconnected with error:", error);
-        }
-        setConnectionStatus("Disconnected");
-        console.log("Disconnected device");
-        setStepCount(0);
-        if (deviceRef.current) {
-          setConnectionStatus("Reconnecting...");
-          connectToDevice(deviceRef.current)
-            .then(() => setConnectionStatus("Connected"))
-            .catch((error) => {
-              console.log("Reconnection failed: ", error);
-              setConnectionStatus("Reconnection failed");
-            });
-        }
-      }
-    );
-    return () => subscription.remove();
-  }, [deviceID]);
 
   return (
     <View style={styles.container}>
-      <Text>{connectionStatus}</Text>
+      <Text>Email: {auth.currentUser?.email}</Text>
+      <TouchableOpacity
+        onPress={handleSignOut}
+        style={styles.button}
+      >
+        <Text style={styles.buttonText}>Sign out</Text>
+      </TouchableOpacity>
     </View>
-  );
+  )
 }
+
+export default HomeScreen
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    justifyContent: 'center',
+    alignItems: 'center'
   },
-});
+   button: {
+    backgroundColor: '#0782F9',
+    width: '60%',
+    padding: 15,
+    borderRadius: 10,
+    alignItems: 'center',
+    marginTop: 40,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: '700',
+    fontSize: 16,
+  },
+})
